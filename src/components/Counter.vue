38f725e6ba2e723e86ab9bd02b744d93e3d007e1
Sergio Kagiema <sergio_kagiema@live.com>
2021-12-06 20:18:51 +0200

    Fix minor bug

--- src/components/Counter.vue
+++ src/components/Counter.vue
@@ -1,13 +1,13 @@
 <template>
   <h2>HOC</h2>
-  <p v-if="counter">{{ counter }}</p>
-  <p v-if="multiply && counter">{{ multiply * counter }}</p>
-  <button @click="increment">add</button>
+  <p v-if="Number.isInteger(counter)">{{ counter }}</p>
+  <p v-if="Number.isInteger(multiply * counter)">{{ multiply * counter }}</p>
+  <button v-if="increment" @click="increment">add</button>
 </template>
 
 <script>
 export default {
-  inheritAttrs: false, // wtf????
+  inheritAttrs: false,
   name: "Counter",
   props: ["counter", "multiply", "increment"],
 };
